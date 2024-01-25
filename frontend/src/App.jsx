1d337d43b54d909ed4289b43de31d105e7fe203c
Bedknobs <noeliareysilva@gmail.com>
2024-01-25 12:26:07 +0100

    estructura inicial

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import {SideBar} from "./components/SideBar/SideBar";
+
+import "primereact/resources/themes/lara-light-cyan/theme.css";
+
+
+export default function App() {
+    return (
+          < SideBar/>
+    );
+}
+        
---
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,37 @@
+import "./SideBar.css";
+
+import { Dropdown } from "primereact/dropdown";
+import { useState } from "react";
+
+import { Button } from 'primereact/button';
+        
+
+export const SideBar = () => {
+  const [selectedCity, setSelectedCity] = useState(null);
+  const cities = [
+    { name: "New York", code: "NY" },
+    { name: "Rome", code: "RM" },
+    { name: "London", code: "LDN" },
+    { name: "Istanbul", code: "IST" },
+    { name: "Paris", code: "PRS" },
+  ];
+
+  return (
+    <div>
+      <img src="../../src/assets/LOGO-CRUDO-APP.svg" />
+      <div className="card flex justify-content-center">
+        <Dropdown
+          value={selectedCity}
+          onChange={(e) => setSelectedCity(e.value)}
+          options={cities}
+          optionLabel="name"
+          placeholder="Select a City"
+          className="w-full md:w-14rem"
+        />
+      </div>
+      <div className="card flex justify-content-center">
+            <Button label="Submit" className="boton"/>
+        </div>
+    </div>
+  );
+};
