b4248bde33e9f3ba3bd201d614ae3fcc79679da3
San-QP <s.rebollar.g@gmail.com>
2024-01-29 13:56:08 +0100

    hover del card

--- frontend/src/components/Panel/Card/Card.jsx
+++ frontend/src/components/Panel/Card/Card.jsx
@@ -1,9 +1,22 @@
+import { useState } from "react";
 import "./Card.css";
 
 export const Card = () => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
   return (
     <section className="cardContainer">
-      <article className="cardGallery">
+      <article className="cardGallery"
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}>
         <div className="cardImage">
           <h3 className="cardName">Nombre de la entrada</h3>
           <img
@@ -12,6 +25,13 @@ export const Card = () => {
             alt="imagen destacada"
           />
         </div>
+          {isHovered && (
+            <div className="cardHover">
+              HOLA HOLA
+            </div>
+          )}
+
+        
       </article>
       <article className="cardGallery emptyCard"></article>
     </section>
