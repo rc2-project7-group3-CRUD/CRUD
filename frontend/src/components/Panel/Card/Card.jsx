cb482082468ad4ec7cad2e026528961886a97f2e
San-QP <s.rebollar.g@gmail.com>
2024-01-29 12:42:33 +0100

    Estilo de h3 card modificado

--- frontend/src/components/Panel/Card/Card.jsx
+++ frontend/src/components/Panel/Card/Card.jsx
@@ -5,12 +5,12 @@ export const Card = () => {
     <section className="cardContainer">
       <article className="cardGallery">
         <div className="cardImage">
+          <h3 className="cardName">Nombre de la entrada</h3>
           <img
             className="image"
             src="../../../../public/imagen-muestra.jpg"
             alt="imagen destacada"
           />
-          <h3>Nombre de la entrada</h3>
         </div>
       </article>
       <article className="cardGallery emptyCard"></article>
