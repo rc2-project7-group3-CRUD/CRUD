dbe40b5266f8ee22c472923c054e3947f13daa6d
laraturrion <laturrions@gmail.com>
2024-01-30 15:06:19 +0100

    article css acabado

--- frontend/src/components/Article/Article.jsx
+++ frontend/src/components/Article/Article.jsx
@@ -1,20 +1,28 @@
 import { Sidebar } from "../Sidebar/Sidebar";
 /*import { useParams } from "react-router-dom";*/
-import './Article.css';
+import "./Article.css";
 
 export const Article = () => {
   /*const { cardId } = useParams();*/
 
   return (
-    <>
+    <section className="mainContainer">
       <Sidebar />
       <div className="articleContainer">
-      <h1>Titulo de la entrada</h1>
-      <p>URL</p>
-      <p className="authorArticle">kjhjk</p>
-      <p className= "dateArticle">jhj</p>
-      <p className="descriptionArticle">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis sequi voluptatibus maxime quos impedit deleniti iure ab laboriosam eius, repellat odit laudantium minima necessitatibus dolorem quae! Aut labore neque obcaecati.</p>
+        <h1 className="titleArticle">Titulo de la entrada</h1>
+        <div className="urlContainer">
+        <i className="pi pi-link linkurl" />
+        <p className="urlArticle">URL</p>
+        </div>
+        <p className="authorArticle">kjhjk</p>
+        <p className="dateArticle">jhj</p>
+        <p className="descriptionArticle">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis
+          sequi voluptatibus maxime quos impedit deleniti iure ab laboriosam
+          eius, repellat odit laudantium minima necessitatibus dolorem quae! Aut
+          labore neque obcaecati.
+        </p>
       </div>
-    </>
+    </section>
   );
 };
