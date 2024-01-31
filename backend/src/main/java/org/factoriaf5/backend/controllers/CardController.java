f229489603fe68c558b3ae9452865f79ab2cb6ab
San-QP <s.rebollar.g@gmail.com>
2024-01-31 14:35:23 +0100

    Delete añadido al backend

--- backend/src/main/java/org/factoriaf5/backend/controllers/CardController.java
+++ backend/src/main/java/org/factoriaf5/backend/controllers/CardController.java
@@ -2,14 +2,19 @@ package org.factoriaf5.backend.controllers;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.factoriaf5.backend.persistence.Card;
 import org.factoriaf5.backend.persistence.CardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -54,6 +59,26 @@ public class CardController {
     //CREAR MÉTODO UPDATE ...
 
     //CREAR MÉTODO DELETE ...
+
+    public class CardNotFoundException extends RuntimeException {
+        public CardNotFoundException(String message) {
+            super(message);
+        }
+    }    
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/cards/{id}")
+    public void deleteCard(@PathVariable Long id) {
+        Optional<Card> card = repository.findById(id);
+
+        //If optional is empty, UserNotFoundException
+        if (card.isEmpty()) {
+            throw new CardNotFoundException("User not found - id:" + id);
+        }
+
+        // Delete card
+        repository.deleteById(id);
+    }
 
     //PARA CONECTAR EL CONTROLADOR AL REPOSITORIO
     private CardRepository repository;
